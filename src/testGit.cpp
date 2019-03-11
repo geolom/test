6c72116d44c2400f5af2059830292c3f8e2de0ed
Georgy Lomakovich <lomakovich47@gmail.com>
2019-03-11 02:04:29 -0700

    Initial commit

---
+++ src/testGit.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : testGit.cpp
+// Author      : lomakovich
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "heya" << endl; // prints hey
+	return 1;
+}
